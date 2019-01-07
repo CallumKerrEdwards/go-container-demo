9eec88637fbd5af2fd300e8837b9fbf58eee5870
Callum Kerr-Edwards <github@callumkerredwards.co.uk>
2019-01-07 14:59:15 +0000

    Simplifying s3 started test further

--- integration/localstack_s3_started_test.go
+++ integration/localstack_s3_started_test.go
@@ -1,12 +1,9 @@
 package integration
 
 import (
-	"bytes"
 	"net/http"
 	"testing"
 )
-
-const S3Endpoint = "http://localhost:4572"
 
 func TestLocalstackS3Started(t *testing.T) {
 	if testing.Short() {
@@ -14,23 +11,19 @@ func TestLocalstackS3Started(t *testing.T) {
 	}
 
 	//given
+	const s3Endpoint = "http://localhost:4572"
 
 	//when
 	//get method is send to s3 localstack endpoint
-	resp, err := http.Get(S3Endpoint)
+	resp, err := http.Get(s3Endpoint)
 	if err != nil {
 		t.Error(err)
 	}
 
-	defer resp.Body.Close()
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(resp.Body)
-	body := buf.String()
-
 	//then
 	//S3 API is available at the given endpoint
 	if resp.StatusCode != 200 {
-		t.Errorf("S3 Endpoint %s was not available:\n"+
-			"Status code was %v, body was %s", S3Endpoint, resp.StatusCode, body)
+		t.Errorf("S3 Endpoint %s was not available:\n Status code was %v",
+			s3Endpoint, resp.StatusCode)
 	}
 }

d0e117a5aeac3c2c5bc95ba6489fbca6a56213f6
Callum Kerr-Edwards <github@callumkerredwards.co.uk>
2019-01-07 13:44:31 +0000

    Simplifying http check for S3 API

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"net/http"
 	"testing"
-	"time"
 )
 
 const S3Endpoint = "http://localhost:4572"
@@ -15,19 +14,10 @@ func TestLocalstackS3Started(t *testing.T) {
 	}
 
 	//given
-	// set up of http client
-	client := http.Client{
-		Timeout: time.Second * 2,
-	}
 
 	//when
 	//get method is send to s3 localstack endpoint
-	req, err := http.NewRequest(http.MethodGet, S3Endpoint, nil)
-	if err != nil {
-		t.Error(err)
-	}
-	req.Header.Set("Accept", "application/json")
-	resp, err := client.Do(req)
+	resp, err := http.Get(S3Endpoint)
 	if err != nil {
 		t.Error(err)
 	}
